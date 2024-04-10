7a14887c8cc76200fb5c4615eb5a8e0f149f60b7
Shivang Rathore <rshivang12345@gmail.com>
2024-04-10 22:41:08 +0530

    feat(server): add auth actions
    Add loginUser and createUser actions, delete temp actions/index.ts

---
+++ actions/auth.ts
@@ -0,0 +1,57 @@
+'use server';
+
+import prisma from '@/prisma/db';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '@prisma/client';
+import { CreateUserSchema, LoginUserSchema } from '@/lib/validation/auth';
+
+export async function createUser(data: CreateUserSchema) {
+  try {
+    var user = await prisma.user.create({
+      data: {
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        credentials: { create: { password: data.password } }
+      }
+    });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code == 'P2002') {
+        return { error: 'Email already exists' };
+      }
+    }
+
+    console.error(e);
+    return { error: 'Something went wrong' };
+  }
+
+  return user;
+}
+
+export async function loginUser(data: LoginUserSchema) {
+  try {
+    var user = await prisma.user.findFirst({
+      where: { email: data.email },
+      include: { credentials: { select: { password: true } } }
+    });
+  } catch (e) {
+    console.error(e);
+    return { error: 'Something went wrong' };
+  }
+
+  if (!user || !user.credentials) {
+    // TODO: Throw a error
+    return { error: 'Invalid email or password' };
+  }
+
+  const match = await bcrypt.compare(data.password, user.credentials.password);
+
+  if (!match) {
+    // TODO: Throw a error
+    return { error: 'Invalid email or password' };
+  }
+
+  const { credentials, ...safe } = user;
+  return safe;
+}
