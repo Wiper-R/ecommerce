e05d20c017b6623c3d119af52ba7747d3797b698
Shivang Rathore <rshivang12345@gmail.com>
2024-04-10 23:35:28 +0530

    feat(ui): add toaster to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '@/components/navbar';
 import { cn } from '@/lib/utils';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={cn(inter.className, 'overflow-hidden flex flex-col')}>
         <Navbar />
         <div className="overflow-auto flex-grow flex flex-col">{children}</div>
+        <Toaster />
       </body>
     </html>
   );
