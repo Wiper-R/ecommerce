a8db26bbf701d77e47c181bc98b7cb44b722cab8
Shivang Rathore <rshivang12345@gmail.com>
2024-04-10 22:46:02 +0530

    chore: delete config directory

--- config/index.ts
+++
@@ -1,5 +0,0 @@
-import { configSchema } from './validation';
-
-const config = configSchema.parse(process.env);
-
-export default config;
--- config/validation.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const configSchema = z.object({
-  DATABASE_URL: z.string().url(),
-  DATABASE_NAME: z.string()
-});
-
-export const clientConfigSchema = z.object({});
