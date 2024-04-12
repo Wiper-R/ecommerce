3921cdb81b2e59bf77bf1eec0a83dea88c9de5f9
Shivang Rathore <rshivang12345@gmail.com>
2024-04-12 23:32:56 +0530

    feat: add category selection menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,50 @@
-import prisma from '@/prisma/db';
+import CustomerElectronicsImage from '@/public/assets/customer-electronics.png';
+import { Card } from '@/components/ui/card';
+import { MaxWidthContainer } from '@/components/containers/max-width-container';
+import Link from 'next/link';
 
 export default async function Home() {
-  return <>Test</>;
+  return (
+    <MaxWidthContainer className="overflow-hidden mt-4">
+      <CategorySelection />
+    </MaxWidthContainer>
+  );
+}
+
+function CategorySelection() {
+  return (
+    <div className="px-4">
+      <div className="overflow-auto flex mt-2 gap-2 mx-auto pb-2">
+        <CategoryItem label="Electronics" />
+        <CategoryItem label="Decoration" />
+        <CategoryItem label="Mobiles" />
+        <CategoryItem label="Groceries" />
+        <CategoryItem label="Electronics" />
+        <CategoryItem label="Decoration" />
+        <CategoryItem label="Mobiles" />
+        <CategoryItem label="Groceries" />
+        <CategoryItem label="Electronics" />
+        <CategoryItem label="Decoration" />
+        <CategoryItem label="Mobiles" />
+        <CategoryItem label="Groceries" />
+        <CategoryItem label="Electronics" />
+        <CategoryItem label="Decoration" />
+        <CategoryItem label="Mobiles" />
+        <CategoryItem label="Groceries" />
+      </div>
+    </div>
+  );
+}
+
+function CategoryItem({ label }: { label: string }) {
+  // TODO: add valid hyperlinks
+  return (
+    <Link
+      href={`/?category=${label}`}
+      className="flex gap-2 shrink-0 py-2 px-4 border rounded items-center"
+    >
+      <img src={CustomerElectronicsImage.src} alt="" className="w-8" />
+      <span className="font-medium text-sm text-center">{label}</span>
+    </Link>
+  );
 }
