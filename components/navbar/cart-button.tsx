ae27f480c1555a521667eac67324e532a9c7546c
Shivang Rathore <rshivang12345@gmail.com>
2024-04-16 10:50:30 +0530

    fix: display real cart

--- components/navbar/cart-button.tsx
+++ components/navbar/cart-button.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import { buttonVariants } from '../ui/button';
 import { ShoppingCartIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
-export function CartButton() {
+// TODO: make items required
+export function CartButton({ items = 0 }: { items: number }) {
   return (
     <div className="relative">
       <Link
@@ -15,8 +17,13 @@ export function CartButton() {
         <ShoppingCartIcon className="w-5" />
         <span>Cart</span>
       </Link>
-      <span className="rounded-full size-6 inline-flex items-center justify-center absolute text-xs -right-2 -bottom-2 bg-primary text-primary-foreground">
-        4
+      <span
+        className={cn(
+          'rounded-full size-6 inline-flex items-center justify-center absolute text-xs -right-2 -bottom-2 bg-primary text-primary-foreground',
+          !items && 'hidden'
+        )}
+      >
+        {items}
       </span>
     </div>
   );
